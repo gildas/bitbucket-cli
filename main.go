1217d23b66c0a790904e94f39d5ec8f5db60fb9d
Gildas Cherruel <gildas@breizh.org>
2023-11-03 23:05:24 +0900

    Read configuration from .env if provided

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "bitbucket.org/gildas_cherruel/bb/cmd"
+import (
+	"bitbucket.org/gildas_cherruel/bb/cmd"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	_ = godotenv.Load()
 	cmd.Execute()
 }
