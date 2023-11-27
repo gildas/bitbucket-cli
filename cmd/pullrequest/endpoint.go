9f5ac811953eda9723eb8cf3f08495d1272ce5ad
Gildas Cherruel <gildas@breizh.org>
2023-11-28 02:17:54 +0900

    Endpoint should use the proper packages

--- cmd/pullrequest/endpoint.go
+++ cmd/pullrequest/endpoint.go
@@ -1,11 +1,12 @@
 package pullrequest
 
 import (
+	"bitbucket.org/gildas_cherruel/bb/cmd/commit"
 	"bitbucket.org/gildas_cherruel/bb/cmd/repository"
 )
 
 type Endpoint struct {
-	Branch     Branch                `json:"branch"     mapstructure:"branch"`
-	Commit     Commit                `json:"commit"     mapstructure:"commit"`
-	Repository repository.Repository `json:"repository" mapstructure:"repository"`
+	Branch     Branch                 `json:"branch"               mapstructure:"branch"`
+	Commit     *commit.Commit         `json:"commit,omitempty"     mapstructure:"commit"`
+	Repository *repository.Repository `json:"repository,omitempty" mapstructure:"repository"`
 }
