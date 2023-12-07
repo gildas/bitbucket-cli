c61926e8a1f87cbe1cf1bc1e634d2159e0f4516c
Gildas Cherruel <gildas@breizh.org>
2023-12-08 00:14:43 +0900

    Updated Bibucket Error struct

--- cmd/profile/error.go
+++ cmd/profile/error.go
@@ -10,7 +10,8 @@ import (
 type BitBucketError struct {
 	Type    string              `json:"type"`
 	Message string              `json:"-"`
-	Detail  map[string][]string `json:"fields"`
+	Detail  string              `json:"-"`
+	Fields  map[string][]string `json:"-"`
 }
 
 func (bberr *BitBucketError) Error() string {
@@ -18,8 +19,12 @@ func (bberr *BitBucketError) Error() string {
 
 	buffer.WriteString(bberr.Message)
 	if len(bberr.Detail) > 0 {
+		buffer.WriteString(": ")
+		buffer.WriteString(bberr.Detail)
+	}
+	if len(bberr.Fields) > 0 {
 		buffer.WriteString(" (")
-		for field, messages := range bberr.Detail {
+		for field, messages := range bberr.Fields {
 			buffer.WriteString(field)
 			buffer.WriteString(": ")
 			buffer.WriteString(strings.Join(messages, ", "))
@@ -33,19 +38,41 @@ func (bberr *BitBucketError) Error() string {
 // UnmarshalJSON unmarshals the JSON
 func (bberr *BitBucketError) UnmarshalJSON(data []byte) (err error) {
 	type surrogate BitBucketError
-	var inner struct {
+
+	var innerType1 struct {
 		surrogate
 		Error struct {
 			Message string              `json:"message"`
-			Detail  map[string][]string `json:"detail"`
-			Fields  map[string][]string `json:"fields"`
+			Fields  map[string][]string `json:"detail"`
 		} `json:"error"`
 	}
-	if err = json.Unmarshal(data, &inner); err != nil {
+	if err = json.Unmarshal(data, &innerType1); err == nil && len(innerType1.Error.Fields) > 0 {
+		*bberr = BitBucketError(innerType1.surrogate)
+		bberr.Message = innerType1.Error.Message
+		bberr.Fields = innerType1.Error.Fields
+		return
+	}
+
+	var innerType2 struct {
+		surrogate
+		Error struct {
+			Message string            `json:"message"`
+			Detail  string            `json:"detail"`
+			Fields  map[string]string `json:"fields"`
+		} `json:"error"`
+	}
+
+	if err = json.Unmarshal(data, &innerType2); err != nil {
 		return errors.JSONUnmarshalError.Wrap(err)
 	}
-	*bberr = BitBucketError(inner.surrogate)
-	bberr.Message = inner.Error.Message
-	bberr.Detail = inner.Error.Detail
+	*bberr = BitBucketError(innerType2.surrogate)
+	bberr.Message = innerType2.Error.Message
+	bberr.Detail = innerType2.Error.Detail
+	if len(innerType2.Error.Fields) > 0 {
+		bberr.Fields = make(map[string][]string)
+		for field, message := range innerType2.Error.Fields {
+			bberr.Fields[field] = []string{message}
+		}
+	}
 	return
 }
---
+++ cmd/profile/profile_test.go
@@ -0,0 +1,83 @@
+package profile_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gildas/go-logger"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/suite"
+)
+
+type ProfileSuite struct {
+	suite.Suite
+	Name   string
+	Logger *logger.Logger
+	Start  time.Time
+}
+
+func TestProfileSuite(t *testing.T) {
+	suite.Run(t, new(ProfileSuite))
+}
+
+// *****************************************************************************
+// Suite Tools
+
+func (suite *ProfileSuite) SetupSuite() {
+	_ = godotenv.Load()
+	suite.Name = strings.TrimSuffix(reflect.TypeOf(suite).Elem().Name(), "Suite")
+	suite.Logger = logger.Create("test",
+		&logger.FileStream{
+			Path:         fmt.Sprintf("./log/test-%s.log", strings.ToLower(suite.Name)),
+			Unbuffered:   true,
+			SourceInfo:   true,
+			FilterLevels: logger.NewLevelSet(logger.TRACE),
+		},
+	).Child("test", "test")
+	suite.Logger.Infof("Suite Start: %s %s", suite.Name, strings.Repeat("=", 80-14-len(suite.Name)))
+}
+
+func (suite *ProfileSuite) TearDownSuite() {
+	suite.Logger.Debugf("Tearing down")
+	if suite.T().Failed() {
+		suite.Logger.Warnf("At least one test failed, we are not cleaning")
+		suite.T().Log("At least one test failed, we are not cleaning")
+	} else {
+		suite.Logger.Infof("All tests succeeded, we are cleaning")
+	}
+	suite.Logger.Infof("Suite End: %s %s", suite.Name, strings.Repeat("=", 80-12-len(suite.Name)))
+}
+
+func (suite *ProfileSuite) BeforeTest(suiteName, testName string) {
+	suite.Logger.Infof("Test Start: %s %s", testName, strings.Repeat("-", 80-13-len(testName)))
+	suite.Start = time.Now()
+}
+
+func (suite *ProfileSuite) AfterTest(suiteName, testName string) {
+	duration := time.Since(suite.Start)
+	if suite.T().Failed() {
+		suite.Logger.Errorf("Test %s failed", testName)
+	}
+	suite.Logger.Record("duration", duration.String()).Infof("Test End: %s %s", testName, strings.Repeat("-", 80-11-len(testName)))
+}
+
+func (suite *ProfileSuite) LoadTestData(filename string) []byte {
+	data, err := os.ReadFile(fmt.Sprintf("../../testdata/%s", filename))
+	if err != nil {
+		suite.T().Fatal(err)
+	}
+	return data
+}
+
+func (suite *ProfileSuite) UnmarshalData(filename string, v interface{}) error {
+	data := suite.LoadTestData(filename)
+	suite.Logger.Infof("Loaded %s: %s", filename, string(data))
+	return json.Unmarshal(data, v)
+}
+
+// *****************************************************************************
