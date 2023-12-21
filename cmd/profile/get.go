bc4a28f343752f3c6663a0709799d330915da290
Gildas Cherruel <gildas@breizh.org>
2023-12-22 03:17:45 +0900

    Get the current profile

--- cmd/profile/get.go
+++ cmd/profile/get.go
@@ -10,17 +10,31 @@ var getCmd = &cobra.Command{
 	Use:               "get",
 	Aliases:           []string{"show", "info", "display"},
 	Short:             "get a profile",
-	Args:              cobra.ExactArgs(1),
 	ValidArgsFunction: ValidProfileNames,
 	RunE:              getProcess,
 }
 
+var getOptions struct {
+	Current bool
+}
+
 func init() {
 	Command.AddCommand(getCmd)
+
+	getCmd.Flags().BoolVar(&getOptions.Current, "current", false, "Get the current profile")
 }
 
 func getProcess(cmd *cobra.Command, args []string) error {
 	log := logger.Must(logger.FromContext(cmd.Context())).Child(cmd.Parent().Name(), "get")
+
+	if getOptions.Current {
+		log.Infof("Displaying current profile")
+		return Current.Print(cmd.Context(), Current)
+	}
+
+	if len(args) == 0 {
+		return errors.ArgumentMissing.With("profile")
+	}
 
 	log.Infof("Displaying profile %s (Valid names: %v)", args[0], Profiles.Names())
 	profile, found := Profiles.Find(args[0])
