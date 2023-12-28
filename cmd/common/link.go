61b7fff237cec9305311a0661b1dd4844f48eac9
Gildas Cherruel <gildas@breizh.org>
2023-12-28 23:51:16 +0900

    New links

--- cmd/common/link.go
+++ cmd/common/link.go
@@ -9,20 +9,32 @@ import (
 )
 
 type Link struct {
-	HREF url.URL `json:"href" mapstructure:"href"`
+	Name   string  `json:"name,omitempty" mapstructure:"name"`
+	HREF   url.URL `json:"-"`
+	GitRef string  `json:"-"`
 }
 
 // MarshalJSON implements the json.Marshaler interface.
 func (link Link) MarshalJSON() (data []byte, err error) {
 	type surrogate Link
 
-	data, err = json.Marshal(struct {
-		surrogate
-		HREF core.URL `json:"href"`
-	}{
-		surrogate: surrogate(link),
-		HREF:      core.URL(link.HREF),
-	})
+	if len(link.GitRef) > 0 {
+		data, err = json.Marshal(struct {
+			surrogate
+			GitRef string `json:"href"`
+		}{
+			surrogate: surrogate(link),
+			GitRef:    link.GitRef,
+		})
+	} else {
+		data, err = json.Marshal(struct {
+			surrogate
+			HREF core.URL `json:"href"`
+		}{
+			surrogate: surrogate(link),
+			HREF:      core.URL(link.HREF),
+		})
+	}
 	return data, errors.JSONMarshalError.Wrap(err)
 }
 
@@ -30,17 +42,40 @@ func (link Link) MarshalJSON() (data []byte, err error) {
 func (link *Link) UnmarshalJSON(data []byte) (err error) {
 	type surrogate Link
 
-	var inner struct {
-		surrogate
-		HREF core.URL `json:"href"`
+	var header struct {
+		Name string `json:"name"`
 	}
-
-	if err = json.Unmarshal(data, &inner); errors.Is(err, errors.JSONUnmarshalError) {
+	if err = json.Unmarshal(data, &header); errors.Is(err, errors.JSONUnmarshalError) {
 		return err
 	} else if err != nil {
 		return errors.JSONUnmarshalError.Wrap(err)
 	}
-	*link = Link(inner.surrogate)
-	link.HREF = inner.HREF.AsURL()
+	switch header.Name {
+	case "ssh":
+		var inner struct {
+			surrogate
+			GitRef string `json:"href"`
+		}
+		if err = json.Unmarshal(data, &inner); errors.Is(err, errors.JSONUnmarshalError) {
+			return err
+		} else if err != nil {
+			return errors.JSONUnmarshalError.Wrap(err)
+		}
+		*link = Link(inner.surrogate)
+		link.GitRef = inner.GitRef
+	default:
+		var inner struct {
+			surrogate
+			HREF core.URL `json:"href"`
+		}
+
+		if err = json.Unmarshal(data, &inner); errors.Is(err, errors.JSONUnmarshalError) {
+			return err
+		} else if err != nil {
+			return errors.JSONUnmarshalError.Wrap(err)
+		}
+		*link = Link(inner.surrogate)
+		link.HREF = inner.HREF.AsURL()
+	}
 	return
 }
