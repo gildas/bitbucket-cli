c6cedb6ea0082ddc1387063b779183323d7ce6c0
Gildas Cherruel <gildas@breizh.org>
2024-10-20 18:41:37 +0900

    Updating packaging

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  file64        = "$toolsDir\bitbucket-cli-0.10.2-windows-amd64.7z"
+  file64        = "$toolsDir\bitbucket-cli-0.11.0-windows-amd64.7z"
   softwareName  = 'bitbucket-cli*'
-  checksum64    = '3a2ad7cb27ce220df6dad1f352fede4de58cf88aea20654b879cf76e31d84079'
+  checksum64    = '56eb42c33a1d9ffd49014b84929f6faab0f7aecfc3914797d38bc65438a851d0'
   checksumType64= 'sha256'
 }
 
