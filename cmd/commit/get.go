4e7b758cc7adaaeb4aac9a3e7c73264149abce55
Gildas Cherruel <gildas@breizh.org>
2026-04-04 17:40:56 +0900

    Added commit get command

---
+++ cmd/commit/get.go
@@ -0,0 +1,73 @@
+package commit
+
+import (
+	"bitbucket.org/gildas_cherruel/bb/cmd/common"
+	"bitbucket.org/gildas_cherruel/bb/cmd/profile"
+	"github.com/gildas/go-flags"
+	"github.com/gildas/go-logger"
+	"github.com/spf13/cobra"
+)
+
+var getCmd = &cobra.Command{
+	Use:               "get [flags] <hash>",
+	Aliases:           []string{"show", "describe"},
+	Short:             "get a commit",
+	Args:              cobra.MaximumNArgs(1),
+	ValidArgsFunction: getValiAdrgs,
+	RunE:              getProcess,
+}
+
+var getOptions struct {
+	Repository string
+	Columns    *flags.EnumSliceFlag
+}
+
+func init() {
+	Command.AddCommand(getCmd)
+
+	getOptions.Columns = flags.NewEnumSliceFlag(columns.Columns()...)
+	getCmd.Flags().StringVar(&getOptions.Repository, "repository", "", "Repository to get a tag from. Defaults to the current repository")
+	getCmd.Flags().Var(getOptions.Columns, "columns", "Comma-separated list of columns to display")
+	_ = getCmd.RegisterFlagCompletionFunc(getOptions.Columns.CompletionFunc("columns"))
+}
+
+func getValiAdrgs(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	if len(args) != 0 {
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+	names, err := GetCommitHashes(cmd.Context(), cmd, args, toComplete)
+	if err != nil {
+		cobra.CompErrorln(err.Error())
+		return []string{}, cobra.ShellCompDirectiveError
+	}
+	return common.FilterValidArgs(names, args, toComplete), cobra.ShellCompDirectiveNoFileComp
+}
+
+func getProcess(cmd *cobra.Command, args []string) (err error) {
+	log := logger.Must(logger.FromContext(cmd.Context())).Child(cmd.Parent().Name(), "get")
+
+	profile, err := profile.GetProfileFromCommand(cmd.Context(), cmd)
+	if err != nil {
+		return err
+	}
+
+	commitName := args[0]
+
+	if commitName == "" {
+		log.Infof("No commit hash provided, getting the latest commit")
+		latestCommit, err := GetLatestCommit(log.ToContext(cmd.Context()), cmd)
+		if err != nil {
+			return err
+		}
+		commitName = latestCommit.Hash
+	}
+
+	log.Infof("Displaying commit %s", commitName)
+	var commit Commit
+
+	err = profile.Get(log.ToContext(cmd.Context()), cmd, "commits/"+commitName, &commit)
+	if err != nil {
+		return err
+	}
+	return profile.Print(cmd.Context(), cmd, commit)
+}
