f733964ab19e1aaab676d4d9350cf843489993cb
Gildas Cherruel <gildas@breizh.org>
2026-04-06 00:09:03 +0900

    Added marshalers

--- cmd/pipeline/target_pullrequest_reference.go
+++ cmd/pipeline/target_pullrequest_reference.go
@@ -1,9 +1,12 @@
 package pipeline
 
 import (
+	"encoding/json"
+
 	"bitbucket.org/gildas_cherruel/bb/cmd/commit"
 	"bitbucket.org/gildas_cherruel/bb/cmd/common"
 	"bitbucket.org/gildas_cherruel/bb/cmd/pullrequest"
+	"github.com/gildas/go-errors"
 )
 
 // PullRequestReferenceTarget represents a target for a pipeline that is a pull request reference.
@@ -39,4 +42,20 @@ func (target PullRequestReferenceTarget) GetDestination() string {
 // implements Target
 func (target PullRequestReferenceTarget) GetCommit() *commit.CommitReference {
 	return target.Commit
+}
+
+// MarshalJSON marshals the PullRequestReferenceTarget to JSON
+//
+// implements json.Marshaler
+func (target PullRequestReferenceTarget) MarshalJSON() ([]byte, error) {
+	type surrogate PullRequestReferenceTarget
+
+	data, err := json.Marshal(struct {
+		Type string `json:"type"`
+		surrogate
+	}{
+		Type:      target.GetType(),
+		surrogate: surrogate(target),
+	})
+	return data, errors.JSONMarshalError.Wrap(err)
 }
--- cmd/pullrequest/pullrequest_reference.go
+++ cmd/pullrequest/pullrequest_reference.go
@@ -1,14 +1,37 @@
 package pullrequest
 
 import (
+	"encoding/json"
+
 	"bitbucket.org/gildas_cherruel/bb/cmd/common"
+	"github.com/gildas/go-errors"
 )
 
 // PullRequestReference describes a reference to a PullRequest
 type PullRequestReference struct {
-	Type    string       `json:"type"  mapstructure:"type"`
 	ID      uint64       `json:"id"    mapstructure:"id"`
-	Title   string       `json:"title" mapstructure:"title"`
-	IsDraft bool         `json:"draft" mapstructure:"draft"`
-	Links   common.Links `json:"links" mapstructure:"links"`
+	Title   string       `json:"title,omitempty" mapstructure:"title"`
+	IsDraft bool         `json:"draft,omitempty" mapstructure:"draft"`
+	Links   common.Links `json:"links,omitempty" mapstructure:"links"`
+}
+
+// MarshalJSON marshals the PullRequestReference to JSON
+//
+// implements json.Marshaler
+func (reference PullRequestReference) MarshalJSON() ([]byte, error) {
+	type surrogate PullRequestReference
+	var links *common.Links
+
+	if !reference.Links.IsEmpty() {
+		links = &reference.Links
+	}
+
+	data, err := json.Marshal(struct {
+		surrogate
+		Links *common.Links `json:"links,omitempty"`
+	}{
+		surrogate: surrogate(reference),
+		Links:     links,
+	})
+	return data, errors.JSONMarshalError.Wrap(err)
 }
