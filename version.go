386c0aba6ef3ad23a9c64f324e758638593a0ce6
Gildas Cherruel <gildas@breizh.org>
2026-04-06 00:15:32 +0900

    Bumped to version 0.17.7

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ var branch string
 var stamp string
 
 // VERSION is the version of this application
-var VERSION = "0.17.6"
+var VERSION = "0.17.7"
 
 // APP is the name of the application
 const APP = "bb"
