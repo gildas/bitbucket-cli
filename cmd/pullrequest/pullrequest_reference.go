ede910c502941a12a54f263992c1e40d56d7b626
Gildas Cherruel <gildas@breizh.org>
2026-05-29 23:29:44 +0900

    Issue #102: Added Commit Reference Target to Pipelines

--- cmd/pullrequest/pullrequest_reference.go
+++ cmd/pullrequest/pullrequest_reference.go
@@ -9,11 +9,12 @@ import (
 
 // PullRequestReference describes a reference to a PullRequest
 type PullRequestReference struct {
-	Type    string       `json:"type"            mapstructure:"type"`
-	ID      uint64       `json:"id"              mapstructure:"id"`
-	Title   string       `json:"title,omitempty" mapstructure:"title"`
-	IsDraft bool         `json:"draft,omitempty" mapstructure:"draft"`
-	Links   common.Links `json:"links,omitempty" mapstructure:"links"`
+	Type     string       `json:"type"             mapstructure:"type"`
+	ID       uint64       `json:"id"               mapstructure:"id"`
+	Title    string       `json:"title,omitempty"  mapstructure:"title"`
+	IsDraft  bool         `json:"draft,omitempty"  mapstructure:"draft"`
+	IsQueued bool         `json:"queued,omitempty" mapstructure:"queued"`
+	Links    common.Links `json:"links,omitempty"  mapstructure:"links"`
 }
 
 // MarshalJSON marshals the PullRequestReference to JSON
