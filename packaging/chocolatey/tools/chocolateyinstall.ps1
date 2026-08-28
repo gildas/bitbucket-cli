9ad1854001677d452cc4ad224230152ed99ded35
Gildas Cherruel <gildas@breizh.org>
2026-08-28 21:48:02 +0900

    chore: Updated Packaging

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType      = 'exe'
   file64        = "$toolsDir\bitbucket-cli-0.18.5-windows-amd64.7z"
   softwareName  = 'bitbucket-cli*'
-  checksum64    = ''
+  checksum64    = 'a91dac25a29b77c801e2ed0da944b300daa90cbbc0afffeea9011a936bb77f54'
   checksumType64= 'sha256'
 }
 
